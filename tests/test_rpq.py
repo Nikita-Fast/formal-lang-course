0f4ff7214908d485037d790337e1a8fab45eaeda
Nikita-Fast <N1kita.fast@yandex.ru>
2022-12-28 17:35:33 +0300

    тесты пересечения булевых матриц и конвертации бм в автомат

--- tests/test_rpq.py
+++ tests/test_rpq.py
@@ -1,5 +1,8 @@
+import networkx as nx
 import numpy as np
 import pytest
+from pyformlang.finite_automaton import NondeterministicFiniteAutomaton, State, Symbol
+
 from tests.utils import read_data_from_json, dot_to_graph
 from project.automata_tools import create_nfa_from_graph
 from project.boolean_matrices import BooleanMatrices
@@ -44,3 +47,67 @@ def test_bm_init(input_bm: BooleanMatrices, expected_bm, start_states, final_sta
 def test_transitive_closure(input_bm: BooleanMatrices, expected_bm):
     tc = input_bm.transitive_closure()
     assert np.array_equal(tc.toarray(), expected_bm)
+
+
+@pytest.mark.parametrize(
+    "input_bm, expected_automaton",
+    read_data_from_json(
+        "test_to_automaton",
+        lambda data: (
+            BooleanMatrices(create_nfa_from_graph(dot_to_graph(data["expected"]))),
+            create_nfa_from_graph(dot_to_graph(data["expected"])),
+        ),
+    ),
+)
+def test_to_automaton(input_bm: BooleanMatrices, expected_automaton):
+    actual = input_bm.to_automaton()
+    assert (
+        nx.drawing.nx_pydot.to_pydot(actual.to_networkx()).__str__()
+        == nx.drawing.nx_pydot.to_pydot(expected_automaton.to_networkx()).__str__()
+    )
+
+
+@pytest.mark.parametrize(
+    "g1, g2, expected",
+    read_data_from_json(
+        "test_intersect",
+        lambda data: (
+            (
+                create_nfa_from_graph(
+                    dot_to_graph(data["graph1"]),
+                    data["start_states1"],
+                    data["final_states1"],
+                )
+            ),
+            (
+                create_nfa_from_graph(
+                    dot_to_graph(data["graph2"]),
+                    data["start_states2"],
+                    data["final_states2"],
+                )
+            ),
+            (
+                create_nfa_from_graph(
+                    dot_to_graph(data["graph_expected"]),
+                    data["start_states_expected"],
+                    data["final_states_expected"],
+                )
+            ),
+        ),
+    ),
+)
+def test_intersect(
+    g1: NondeterministicFiniteAutomaton,
+    g2: NondeterministicFiniteAutomaton,
+    expected: NondeterministicFiniteAutomaton,
+):
+    bm1 = BooleanMatrices(g1)
+    bm2 = BooleanMatrices(g2)
+    intersection = bm1.intersect(bm2)
+
+    actual = intersection.to_automaton()
+
+    if len(expected.states) == 0:
+        assert actual.to_networkx().__str__() == expected.to_networkx().__str__()
+    else:
+        assert actual.is_equivalent_to(expected)
